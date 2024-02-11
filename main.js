7f677a1ba2906f21a738af547ee0325315376d5b
meheru273 <meherujannat@gmail.com>
2024-02-11 15:46:22 +0600

    day2

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-const navMenu = document.getElementById('nav-menu'),
-navtoggle=document.getElementById('nav-toggle'),
-navClose=document.getElementById('nav-close')
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
 
-
-if(navtoggle){
-    navtoggle.addEventListener('click',()=>
-    {
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
 }
 
-if(navClose){
-    navClose.addEventListener('click',()=>{
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
 }
 
 const navLinks = document.querySelectorAll('.nav_link');
@@ -24,3 +22,91 @@ function linkAction() {
 }
 
 navLinks.forEach(link => link.addEventListener('click', linkAction));
+
+const skillsContent = document.getElementsByClassName('skills_content');
+const skillsHeader = document.querySelectorAll('.skills_header'); // Add '.' before 'skills_header'
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills_content skills_close';
+    }
+    if (itemClass === 'skills_content skills_close') {
+        this.parentNode.className = 'skills_content skills_open';
+    }
+}
+
+skillsHeader.forEach(el => {
+    el.addEventListener('click', toggleSkills);
+});
+
+const tabs = document.querySelectorAll('[data-target]'),
+      tabsContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.getAttribute('data-target'));
+
+    tabsContents.forEach(tabContent => {
+      tabContent.classList.remove('qualification_active');
+    });
+    tabs.forEach(t => {
+      t.classList.remove('qualification_active');
+    });
+
+    target.classList.add('qualification_active');
+    tab.classList.add('qualification_active');
+  });
+});
+
+const modalViews=document.querySelectorAll('.services_modal'),
+modalBtns=document.querySelectorAll('.services_button'),
+modalCloses=document.querySelectorAll('.services_modal-close')
+
+let modal=function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn,i)=>{
+    modalBtn.addEventListener('click',()=>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose)=>{
+    modalClose.addEventListener('click',()=>{
+        modalViews.forEach((modalView)=>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
+
+let swiperportfolio = new Swiper('.portfolio_container', {
+    cssMode: true,
+    loop:true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+let swipertestimonial = new Swiper('.testimonial_container', {
+    loop:true,
+    grabCursor: true,
+    spaceBetween: 48,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+});
+
